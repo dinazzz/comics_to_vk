1b30d2478b4b2a31848304af9ad01cce6c73d5e8
Dinazzz <dinazval@gmail.com>
2019-05-05 14:33:58 +0500

    Исправлен баг, связанный с неверным форматом group_id.
    Т.к. group_id считывается из .env, то переменной присваивается значения типа 'str', хотя дальнейшая работа ведется с ней как с int-объектом.
    Баг устранен приведением group_id к целочисленному типу.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from xkcd_processes import download_random_comics
 if __name__ == '__main__':
     load_dotenv()
     access_token = getenv('ACCESS_TOKEN')
-    group_id = getenv('GROUP_ID')
+    group_id = int(getenv('GROUP_ID'))
     directory = 'comics'
 
     file_path, message = download_random_comics(directory)
